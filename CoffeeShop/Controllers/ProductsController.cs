9b54772cc3f98190a1e6247449797ced647651b2
ADMIN <ADMIN@ACER>
2025-05-19 14:06:39 +0700

    Add project files.

---
+++ CoffeeShop/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using YourProjectNamespace.Repositories;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = productRepository.GetProductDetail(id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ CoffeeShop/Models/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace CoffeeShop.Models
+{
+    public class ProductRepository
+    {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Espresso", Detail = "Strong coffee", ImageUrl = "/images/espresso.jpg", Price = 2.99m },
+            new Product { Id = 2, Name = "Latte", Detail = "Milk coffee", ImageUrl = "/images/latte.jpg", Price = 3.99m }
+        };
+
+        public Product GetProductDetail(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
